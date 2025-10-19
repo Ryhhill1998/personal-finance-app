93c5addea998fa9124ef464aa9bd595f4632fbfe
ryhhill1998 <ryhhill1998@outlook.com>
2025-10-19 10:17:43 +0100

    wrote base routes to get started

--- backend/src/main.py
+++ backend/src/main.py
@@ -13,3 +13,23 @@ app = FastAPI()
 async def root(settings: Annotated[Settings, Depends(get_settings)]):
     print(settings)
     return {"message": "Hello World"}
+
+
+@app.get("/transactions/{bank}/{year}/{month}")
+async def get_bank_transactions_on_date(bank: str, year: int, month: int):
+    return {"transactions": []}
+
+
+@app.get("/transactions/{year}/{month}")
+async def get_all_transactions_on_date(year: int, month: int):
+    return {"transactions": []}
+
+
+@app.get("/transactions")
+async def get_all_transactions():
+    return {"transactions": []}
+
+
+@app.post("/process-statement")
+async def process_statement(statement: dict):
+    return {"status": "processing"}
