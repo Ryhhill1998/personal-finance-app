099052f04e3da2bfee41f684b7806ca31b141b3f
ryhhill1998 <ryhhill1998@outlook.com>
2025-10-25 12:21:35 +0100

    implemented get statement method

--- backend/src/services/storage/local_storage_service.py
+++ backend/src/services/storage/local_storage_service.py
@@ -8,17 +8,22 @@ class LocalStorageService(StorageService):
     def __init__(self, storage_dir_path: Path):
         self.storage_dir_path = storage_dir_path
 
+    def get_statement_dir_path(self, bank_name: str) -> Path:
+        return self.storage_dir_path / bank_name / "raw"
+
     def store_statement(self, statement_bytes: bytes, bank_name: str, year: int, month: int) -> None:
-        dir_path = self.storage_dir_path / bank_name / "raw"
+        dir_path = self.get_statement_dir_path(bank_name)
         file_path =  dir_path / f"Statement_{year}_{month}.pdf"
         dir_path.mkdir(parents=True, exist_ok=True)
         file_path.write_bytes(statement_bytes)
 
     def get_statement(self, bank_name: str, year: int, month: int) -> str:
-        pass
+        dir_path = self.get_statement_dir_path(bank_name)
+        file_path = dir_path / f"Statement_{year}_{month}.pdf"
+        return file_path.read_text()
 
     def store_parsed_transactions(
-            self, parsed_transactions: ParsedTransactions, bank_name: str, year: int, month: int
+        self, parsed_transactions: ParsedTransactions, bank_name: str, year: int, month: int
     ) -> None:
         pass
 
