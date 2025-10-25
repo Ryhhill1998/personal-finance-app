6e82f77b21a2ce6af6f3d472d6afbc03617c3bd1
ryhhill1998 <ryhhill1998@outlook.com>
2025-10-25 12:28:27 +0100

    implemented store parsed transactions method

--- backend/src/services/storage/local_storage_service.py
+++ backend/src/services/storage/local_storage_service.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 from src.models import ParsedTransactions
@@ -8,24 +9,26 @@ class LocalStorageService(StorageService):
     def __init__(self, storage_dir_path: Path):
         self.storage_dir_path = storage_dir_path
 
-    def get_statement_dir_path(self, bank_name: str) -> Path:
-        return self.storage_dir_path / bank_name / "raw"
-
     def store_statement(self, statement_bytes: bytes, bank_name: str, year: int, month: int) -> None:
-        dir_path = self.get_statement_dir_path(bank_name)
-        file_path =  dir_path / f"Statement_{year}_{month}.pdf"
+        dir_path = self.storage_dir_path / bank_name / "raw"
         dir_path.mkdir(parents=True, exist_ok=True)
+        file_path = dir_path / f"Statement_{year}_{month}.pdf"
         file_path.write_bytes(statement_bytes)
 
     def get_statement(self, bank_name: str, year: int, month: int) -> str:
-        dir_path = self.get_statement_dir_path(bank_name)
+        dir_path = self.storage_dir_path / bank_name / "raw"
         file_path = dir_path / f"Statement_{year}_{month}.pdf"
         return file_path.read_text()
 
     def store_parsed_transactions(
         self, parsed_transactions: ParsedTransactions, bank_name: str, year: int, month: int
     ) -> None:
-        pass
+        dir_path = self.storage_dir_path / bank_name / "parsed"
+        dir_path.mkdir(parents=True, exist_ok=True)
+        file_path = dir_path / f"Statement_{year}_{month}.json"
+
+        with open(file_path, "w") as parsed_file:
+            json.dump(parsed_transactions.model_dump_json(), parsed_file)
 
     def get_parsed_transactions(self, bank_name: str, year: int, month: int) -> ParsedTransactions:
         pass
