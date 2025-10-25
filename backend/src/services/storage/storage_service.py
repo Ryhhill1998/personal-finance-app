dc4fc2cc56df17eae16ddc612446634b53cfcfb5
ryhhill1998 <ryhhill1998@outlook.com>
2025-10-25 12:15:46 +0100

    added store statement method to storage service

--- backend/src/services/storage/storage_service.py
+++ backend/src/services/storage/storage_service.py
@@ -1,12 +1,17 @@
+from pathlib import Path
+
 from src.models import ParsedTransactions
 
 
 class StorageService:
-    def __init__(self):
-        pass
+    def __init__(self, storage_dir_path: Path):
+        self.storage_dir_path = storage_dir_path
 
-    def store_statement(self, statement_bytes: bytes, storage_path: str) -> None:
-        pass
+    def store_statement(self, statement_bytes: bytes, bank_name: str, year: int, month: int) -> None:
+        dir_path = self.storage_dir_path / bank_name / "raw"
+        file_path =  dir_path / f"Statement_{year}_{month}.pdf"
+        dir_path.mkdir(parents=True, exist_ok=True)
+        file_path.write_bytes(statement_bytes)
 
     def get_statement(self) -> str:
         pass
