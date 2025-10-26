32760738dca4ce336f5ffd6489c11aa9e10696d7
ryhhill1998 <ryhhill1998@outlook.com>
2025-10-26 10:04:34 +0000

    added test for this route

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -1,3 +1,4 @@
+import json
 from datetime import date
 from io import BytesIO
 from pathlib import Path
@@ -12,6 +13,7 @@ from src.models import ParsedTransactions, Transaction
 from src.settings import Settings
 
 
+# -------------------- FIXTURES -------------------- #
 @pytest.fixture
 def override_get_settings(tmp_path: Path) -> Generator[None, Any, None]:
     test_settings = Settings(
@@ -65,11 +67,50 @@ def override_get_model_service(tmp_path: Path) -> Generator[None, Any, None]:
     app.dependency_overrides = {}
 
 
-def test_get_bank_transactions_on_date():
+# -------------------- TESTS -------------------- #
+def test_get_bank_transactions_on_date(tmp_path: Path, override_get_settings: None) -> None:
+    # ARRANGE
+    bank_name = "Test Bank"
+    json_data = {
+        "transactions": [
+            {
+                "date": "2025-01-01",
+                "description": "Transaction 1",
+                "amount_in": 100,
+                "amount_out": 0,
+                "balance": 500,
+            },
+            {
+                "date": "2025-01-05",
+                "description": "Transaction 2",
+                "amount_in": 0,
+                "amount_out": 150,
+                "balance": 350,
+            },
+            {
+                "date": "2025-01-21",
+                "description": "Transaction 3",
+                "amount_in": 1000,
+                "amount_out": 0,
+                "balance": 1350,
+            },
+        ]
+    }
+
+    dir_path = tmp_path / bank_name / "parsed"
+    dir_path.mkdir(parents=True, exist_ok=True)
+
+    with open(dir_path / f"Statement_2025_01.json", "w") as parsed_transactions_file:
+        json.dump(json_data, parsed_transactions_file)
+
     client = TestClient(app)
-    response = client.get("/transactions/bank/{bank_name}/{year}/{month}")
+
+    # ACT
+    response = client.get(f"/transactions/bank/{bank_name}/2025/1")
+
+    # ASSERT
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+    assert response.json() == json_data
 
 
 def test_get_all_transactions_on_date():
