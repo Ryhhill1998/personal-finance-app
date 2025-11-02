957572f093e4e94c9fcc7cbafc162d7ade827f7d
ryhhill1998 <ryhhill1998@outlook.com>
2025-11-02 09:55:24 +0000

    moved statement processing to own route file

--- backend/src/main.py
+++ backend/src/main.py
@@ -62,33 +62,3 @@ async def get_transactions_for_bank_for_date(
         return storage_service.get_transactions_for_bank_for_date(bank_name=bank_name, year=year, month=month)
     except StorageServiceException:
         raise HTTPException(status_code=404, detail="Cannot find requested file")
-
-
-@app.post("/process-statement/{bank_name}/{year}/{month}")
-async def process_statement(
-    bank_name: str,
-    year: int,
-    month: int,
-    statement: UploadFile,
-    statement_parser: Annotated[ModelStatementParser, Depends(get_model_statement_parser)],
-    storage_service: Annotated[StorageService, Depends(get_local_storage_service)],
-):
-    """
-    Processes the uploaded statement and stores the result.
-    Also stores the raw uploaded file.
-    Does not return the result - this must be retrieved by one of the above GET methods.
-    """
-
-    # extract bytes from uploaded statement
-    statement_bytes = statement.file.read()
-
-    # store uploaded statement
-    storage_service.store_statement(statement_bytes=statement_bytes, bank_name=bank_name, year=year, month=month)
-
-    # get json data from pdf via model
-    transactions = await statement_parser.parse_transactions(bank_name=bank_name, statement=statement_bytes.decode())
-
-    # store parsed transactions
-    storage_service.store_transactions(
-        transactions=transactions, bank_name=bank_name, year=year, month=month
-    )
---
+++ backend/src/routers/statements.py
@@ -0,0 +1,39 @@
+from typing import Annotated
+
+from fastapi import APIRouter, UploadFile, Depends
+
+from src.dependencies import get_local_storage_service, get_model_statement_parser
+from src.services.statement_parser.model_statement_parser import ModelStatementParser
+from src.services.storage.storage_service import StorageService
+
+router = APIRouter(prefix="statements")
+
+
+@router.post("/{bank_name}/{year}/{month}")
+async def upload_statement(
+    bank_name: str,
+    year: int,
+    month: int,
+    statement: UploadFile,
+    statement_parser: Annotated[ModelStatementParser, Depends(get_model_statement_parser)],
+    storage_service: Annotated[StorageService, Depends(get_local_storage_service)],
+):
+    """
+    Processes the uploaded statement and stores the result.
+    Also stores the raw uploaded file.
+    Does not return the result - this must be retrieved by one of the above GET methods.
+    """
+
+    # extract bytes from uploaded statement
+    statement_bytes = statement.file.read()
+
+    # store uploaded statement
+    storage_service.store_statement(statement_bytes=statement_bytes, bank_name=bank_name, year=year, month=month)
+
+    # get json data from pdf via model
+    transactions = await statement_parser.parse_transactions(bank_name=bank_name, statement=statement_bytes.decode())
+
+    # store parsed transactions
+    storage_service.store_transactions(
+        transactions=transactions, bank_name=bank_name, year=year, month=month
+    )
